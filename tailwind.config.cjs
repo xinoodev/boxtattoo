d5a8ef96340b63de0cd3526a2867db4edee666c0
xinoodev <errxino.soporte@gmail.com>
2026-08-11 14:14:41 +0200

    chore: add initial project configuration and public files

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        neonpink: '#ff2fa6'
+      },
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui']
+      }
+    }
+  },
+  plugins: []
+}
